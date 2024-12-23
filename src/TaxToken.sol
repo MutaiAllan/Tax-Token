69440ee986f45b37c9e9dd108a449860ae2a133d
MutaiAllan <allankiprop175@gmail.com>
2024-12-23 22:05:22 +0300

    Added transfer function

--- src/TaxToken.sol
+++ src/TaxToken.sol
@@ -4,9 +4,33 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TaxToken is ERC20 {
+    // 10% tax
+    uint256 taxDivisor = 10;
 
     // Calling the ERC20 constructor in the imported ERC20.
-    constructor() ERC20("TaxToken", "TT") {
+    constructor() ERC20("TaxToken", "TT") {}
 
+    function mintToMe(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
+    function transfer(address to, uint amount) public override returns (bool) {
+        // Get the balance of the address calling the contract.
+        uint balanceSender = balanceOf(msg.sender);
+        // Check if the balance of the sender( address calling the contract) is greater than the amount to be transferred.
+        require(balanceSender >= amount, "Insufficient balance to transer!");
+
+        // Get the tax amount(10% of the amount to be transfered)
+        uint taxAmount = amount / taxDivisor; // The tax amount doesn't go anywhere, it stays in the contract.
+        // AMount to be transferred
+        uint transferAmount = amount - taxAmount;
+
+        // Subract the transfer amount from the sender's balance
+        _balances[msg.sender] -= amount;
+        // Add the amoun tto the balance of the recipient
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+        return true;
     }
 }
